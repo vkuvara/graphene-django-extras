814cea005fd7489ff213462c2d528d056a3a25e2
vkuvara <vkuvara@users.noreply.github.com>
2018-12-12 14:54:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,8 +56,6 @@ setup(
 
     install_requires=[
         'graphql-core==2.0',
-        'graphene==2.0.1',
-        'graphene-django==2.0',
         'django-filter==1.1.0',
         'djangorestframework>=3.6.0'
     ],
